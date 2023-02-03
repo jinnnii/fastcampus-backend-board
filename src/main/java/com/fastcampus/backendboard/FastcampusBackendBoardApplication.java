78776dde366c2e94cc61a31decf588769ff0d5ea
jinnnii <jinnnii@naver.com>
2023-02-03 21:09:38 +0900

    #5 - create spring boot project
    Dependencies : 
    * Spring boot DevTools
    * lombok
    * Spring Web
    * Spring Actuator

---
+++ src/main/java/com/fastcampus/backendboard/FastcampusBackendBoardApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.backendboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastcampusBackendBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastcampusBackendBoardApplication.class, args);
+    }
+
+}
