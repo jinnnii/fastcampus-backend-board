6cff1356aa577d0e11e5d04b3558ea320e4c79d5
jinnnii <jinnnii@naver.com>
2023-02-12 21:55:12 +0900

    #10 - set spring data jpa config
    * add 'JpaConfig' to do jpa auditing function
    * add propreties for db access & convenience
    * TODO : password was exposed !

---
+++ src/main/java/com/fastcampus/backendboard/config/JpaConfig.java
@@ -0,0 +1,18 @@
+package com.fastcampus.backendboard.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+    @Bean
+    public AuditorAware<String> auditorAware(){
+        return ()-> Optional.of("kej"); //// TODO: 2023-02-07 Fix after applying Spring Security 
+    }
+}
