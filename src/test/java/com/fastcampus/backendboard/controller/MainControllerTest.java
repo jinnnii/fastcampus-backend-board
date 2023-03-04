8b21f005af4c7c73b0db1c5e658bb9fe51729e81
jinnnii <jinnnii@naver.com>
2023-03-04 18:25:41 +0900

    #20 - set root page to article page
    make 'MainController'
    make default page article page with 'forward'

---
+++ src/test/java/com/fastcampus/backendboard/controller/MainControllerTest.java
@@ -0,0 +1,33 @@
+package com.fastcampus.backendboard.controller;
+
+import com.fastcampus.backendboard.config.SecurityConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+@DisplayName("Root Controller test")
+@Import(SecurityConfig.class)
+@WebMvcTest(MainController.class)
+class MainControllerTest {
+    private final MockMvc mvc;
+
+    public MainControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("[view][GET] Home page -200 OK")
+    @Test
+    void givenNothing_whenRequestingRootPage_thenRedirectsToArticlesPage() throws Exception {
+        //Given
+
+        //When & Then
+        mvc.perform(get("/"))
+                .andExpect(status().is3xxRedirection());
+    }
+}
